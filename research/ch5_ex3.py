a6b303b817440482cbb23d918b172ade602c7cbf
konijntjesbroek <proticon@gmail.com>
2026-01-25 14:13:50 -0600

    janky fileshare

---
+++ research/ch5_ex3.py
@@ -0,0 +1,22 @@
+# Chapter 5, challenge 3
+# created by: Arlo Gittings
+# created on: 2026.01.24
+# updated on: 2026.01.24
+# prompt:
+#   Write a Who's Your Daddy? prgram that lets the user enter the name of male
+#   and produces the name of his father. (You can use celebrities, fictional
+#   characters, or even historical figures for fun) Allow the user to add,
+#   replace, and delete son-father pairs.
+
+dudes = { # "Dude": (dude_id, dad_id)
+        "God":  (0, 0),
+        "Adam": (1, 0),
+        "Cain": (2, 1),
+        "Abel": (3, 1),
+        "Seth": (4, 1),
+        "Methuselah": (5, 4),
+        "Noah": (6, 5),
+        "Shem": (7, 6),
+        "Ham": (8, 6),
+        "Japheth": (9, 6)}
+`
